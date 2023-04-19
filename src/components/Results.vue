1313e349cc42862b3f57973c8bc11ea9389e4049
Aniket Gupta <Aniket.g@greyorange.com>
2023-04-19 15:53:54 +0530

    Reactiom timer project in Vue.js

---
+++ src/components/Results.vue
@@ -0,0 +1,28 @@
+<template>
+  <h3>THE SCORE IS {{ this.score }} ms </h3>
+  <h3>You are {{ rank }}</h3>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data(){
+        return{
+            rank : null
+        }
+    },
+    mounted(){
+       if(this.score < 300){
+            this.rank = "Khiladi"
+       }else if (this.score < 500 ){
+            this.rank = "Average"
+       }else{
+         this.rank = "Anadi" 
+       }
+    }
+}
+</script>
+
+<style>
+
+</style>
